b929f19ec6c610c6db220d2480fe3cfe636de1e4
sanjarbek <sanjarsaidov173@gamil.com>
2022-09-10 08:29:55 +0500

    signals added

--- databa/users/views.py
+++ databa/users/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, "Your account has been crated!")
+            messages.success(request, "Your account has been created!")
             return redirect('login')
     else: 
         form = UserRegisterFrom()
