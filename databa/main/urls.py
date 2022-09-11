63935898b7e00a079e8fae44a31b6190f9ca0932
sanjarbek <sanjarsaidov173@gamil.com>
2022-09-11 19:15:50 +0500

    CBV CRUD

--- databa/main/urls.py
+++ databa/main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home, about
+from .views import *
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about', about, name='blog-about'),
 ]
--- databa/main/views.py
+++ databa/main/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import Post
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'pages/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'pages/post_detail.html'
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    template_name = 'pages/post_delete.html'
+    
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'pages/post_create.html'
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'pages/post_create.html'
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+    
 def home(request):
     context = {
         'posts': Post.objects.all()
